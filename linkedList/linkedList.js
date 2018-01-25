8a9a2e51cfa816e4e8cbfc34b115c02e314a80f7
Ely Alamillo <ely0013@gmail.com>
2018-01-25 00:31:37 -0600

    worked on reversingList

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -36,15 +36,38 @@ class linkedList {
   }
 }
 
+const reverse = linkedList => {
+  let result = null;
+  const stack = [];
+  let current = linkedList.head;
+
+  while (current) {
+    stack.push(current);
+    current = current.next;
+    console.log(stack);
+  }
+
+  result = stack.pop();
+  current = result;
+  while (current) {
+    current.next = stack.pop();
+    current = current.next;
+  }
+  return result;
+};
+
 const list = new linkedList();
 list.addToTail(10);
 list.addToTail(20);
 list.addToTail(30);
 list.addToTail(40);
+list.addToTail(50);
+// console.log(JSON.stringify(list));
+// list.removeHead();
+// console.log(JSON.stringify(list));
+// console.log(list.contains(20));
+// console.log(list.contains(10));
 console.log(JSON.stringify(list));
-list.removeHead();
-console.log(JSON.stringify(list));
-console.log(list.contains(20));
-console.log(list.contains(10));
+console.log(JSON.stringify(reverse(list)));
 
 module.exports = linkedList;
